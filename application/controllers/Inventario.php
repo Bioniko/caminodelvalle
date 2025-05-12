ffa953b3ac447155845bceab7ffa22e49163b56b
Bioniko <85115922+Bioniko@users.noreply.github.com>
2025-05-12 15:40:25 -0600

    Update Inventario.php

--- application/controllers/Inventario.php
+++ application/controllers/Inventario.php
@@ -92,15 +92,15 @@ class Inventario extends CI_Controller {
 			$inv = $this->db->query($Inventario);
 			if ($this->db->affected_rows() > 0) {
 				echo "<script>alert('Actualización realizada correctamente.');</script>";
-				$Inventario = "SELECT * FROM inventario WHERE  ID_Sucursal = ".$_COOKIE['suc_id'];
-				$inv = $this->db->query($Inventario)->result();
-				$data = (object)array('inv' => $inv);
-				$this->load->view('1inventarionuevo.php',(array)$data);
 			} else {
 				echo "<script>alert('No se realizaron cambios. Verifica los datos proporcionados.');</script>";
 			}
 		} catch (Exception $e) {
 			echo "<script>alert('Ocurrió un error al actualizar: " . $e->getMessage() . "');</script>";
 		}
+		$Inventario = "SELECT * FROM inventario WHERE  ID_Sucursal = ".$_COOKIE['suc_id'];
+		$inv = $this->db->query($Inventario)->result();
+		$data = (object)array('inv' => $inv);
+		$this->load->view('1inventarionuevo.php',(array)$data);
 	}	
 }
